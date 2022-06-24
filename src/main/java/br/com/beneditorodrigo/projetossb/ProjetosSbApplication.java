d808b0f4eb6c0a5acf2267a56d3abcb2ffa97894
beneditorodrigo <rodrigodacunha7@gmail.com>
2022-06-24 02:47:12 -0300

    Criação do projeto e teste com primeiro Web Service

---
+++ src/main/java/br/com/beneditorodrigo/projetossb/ProjetosSbApplication.java
@@ -0,0 +1,13 @@
+package br.com.beneditorodrigo.projetossb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetosSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetosSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/beneditorodrigo/projetossb/ProjetosSbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.beneditorodrigo.projetossb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetosSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
