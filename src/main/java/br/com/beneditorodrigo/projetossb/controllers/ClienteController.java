681ca9553117e7f721b6f9c1c79e30d619284a77
beneditorodrigo <rodrigodacunha7@gmail.com>
2022-06-25 01:21:13 -0300

    Passando parâmetros para Web Services

--- src/main/java/br/com/beneditorodrigo/projetossb/controllers/ClienteController.java
+++ src/main/java/br/com/beneditorodrigo/projetossb/controllers/ClienteController.java
@@ -1,7 +1,9 @@
 package br.com.beneditorodrigo.projetossb.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.beneditorodrigo.projetossb.models.Cliente;
@@ -13,5 +15,16 @@ public class ClienteController {
 	@GetMapping("/qualquer")
 	public Cliente obterCliente() {
 		return new Cliente(28, "Rodrigo", "123.654.789-10");
+	}
+	
+	@GetMapping("/{id}")
+	public Cliente obterClientePorId1(@PathVariable int id) {
+		return new Cliente(id, "Maria", "987.456.321-10");
+	}
+	
+	@GetMapping
+	public Cliente obterClientePorId2(
+			@RequestParam(name = "id", defaultValue = "1") int id) {
+		return new Cliente(id, "Pedro Roma", "258.369.741-01");
 	}
 }
